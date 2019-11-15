295025e2da0b641efed6a81ea38159acba1cb408
nandoogouveia <gouveiaramos.me@gmail.com>
2019-11-15 21:28:46 +0000

    ajustes 5 - templetes e home.hs

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -11,25 +11,70 @@ import Text.Lucius
 import Text.Julius
 --import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
--- teste
+
+getPage2R :: Handler Html 
+getPage2R = do 
+    defaultLayout $ do 
+        $(whamletFile "templates/page2.hamlet")
+
+getPage1R :: Handler Html 
+getPage1R = do 
+    defaultLayout $ do 
+        addStylesheet (StaticR css_bootstrap_css)
+        toWidgetHead $(juliusFile "templates/page1.julius")
+        toWidgetHead $(luciusFile "templates/page1.lucius")
+        $(whamletFile "templates/page1.hamlet")
+
 getHomeR :: Handler Html
-getHomeR = do
-    defaultLayout $ do
-        toWidgetHead $(juliusFile "templates/banner.julius")
-        toWidgetHead $(luciusFile "templates/style.lucius")
-        $(whamletFile "templates/header.hamlet")
-        $(whamletFile "templates/homepage.hamlet")
-        $(whamletFile "templates/footer.hamlet")
+getHomeR = do 
+    defaultLayout $ do 
+        -- remoto
+        addScriptRemote "https://code.jquery.com/jquery-3.4.1.min.js"
+        -- esta no projeto
+        addStylesheet (StaticR css_bootstrap_css)
+        sess <- lookupSession "_NOME"
+        toWidgetHead [julius|
+            function ola(){
+                alert("OLA MUNDO");
+            }
+        |]
+        toWidgetHead [lucius|
+            h1 {
+                color : red;
+            }
             
---getAdsR :: Handler TypedContent
---getAdsR = return $ TypedContent "text/plain"
---    $ toContent $(embedFile "static/ads.txt")
-    
-getPostsR :: Handler Html
-getPostsR = do
-    defaultLayout $ do
-        toWidgetHead $(juliusFile "templates/banner.julius")
-        toWidgetHead $(luciusFile "templates/style.lucius")
-        $(whamletFile "templates/header.hamlet")
-        $(whamletFile "templates/posts.hamlet")
-        $(whamletFile "templates/footer.hamlet")
+            ul {
+                display: inline;
+                list-style: none;
+            }
+        |]
+        [whamlet|
+            <div>
+                <h1>
+                    OLA MUNDO
+            
+            <ul>
+                <li>
+                    <a href=@{Page1R}>
+                        Pagina 1
+                
+                <li>
+                    <a href=@{Page2R}>
+                        Pagina 2
+                
+                $maybe nome <- sess
+                    <li>
+                        <div>
+                            Ola #{nome}
+                        <form method=post action=@{SairR}>
+                            <input type="submit" value="Sair">
+                $nothing
+                    <li>
+                        <div>
+                            convidado
+            
+            <img src=@{StaticR citeg_jpg}>
+            
+            <button class="btn btn-danger" onclick="ola()">
+                OK
+        |]
